059014569fa9eebbbed751056ac9440a017f69ba
Matías Monastirsky <81826117+MatiMonas@users.noreply.github.com>
2022-08-18 21:07:51 -0300

    delte example

--- routes/users.js
+++ routes/users.js
@@ -2,21 +2,12 @@ var express = require('express');
 var router = express.Router();
 const db = require('../models')
 
-// const { Role } = db.sequelize.models
 /* GET users listing. */
 
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-// router.get('/', async (req, res ) =>  {
-//   try {
-//     const newRole = await Role.create({name: 'prueba'})
-//     return res.json(newRole)
-    
-//   } catch (error) {
-//     return res.status(400)
-//   }
-// })
+
 
 module.exports = router;
