eda5b645a0e9726f87f5cd18f053b6c85303632b
Esteban <contacto@egdesarrollos.com.ar>
2022-08-19 19:02:54 -0300

    server app.js conflitc solved

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-
+const categoryRouter = require('./routes/category');
 const app = express();
 app.use(cors());
 
@@ -24,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/category', categoryRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
