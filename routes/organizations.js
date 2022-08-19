05007507aaa8bbc5b9e001823650b81db343b68a
Matías Monastirsky <81826117+MatiMonas@users.noreply.github.com>
2022-08-19 18:16:49 -0300

    add organization controller

--- routes/organizations.js
+++ routes/organizations.js
@@ -1,9 +1,8 @@
 var express = require('express');
+const { getOrganizationController } = require('../controllers/organization');
 var router = express.Router();
 
 /* GET organizations listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', getOrganizationController);
 
 module.exports = router;
