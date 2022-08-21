85577a5df9162057f8f4fa57c5f87da845af7d09
Matías Monastirsky <81826117+MatiMonas@users.noreply.github.com>
2022-08-21 01:52:50 -0300

    updates error handler

--- app.js
+++ app.js
@@ -39,8 +39,9 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res
+  .status(err.status || 500)
+  .json({ message:res.locals.message, statusCode: err.status});
 });
 
 module.exports = app;
