4b54386039ebd54ac126ff6fd5171d487d254672
pablodclavijo <76256398+pablodclavijo@users.noreply.github.com>
2022-08-20 22:56:10 -0300

    model + migration + controller

---
+++ controllers/members.controller.js
@@ -0,0 +1,61 @@
+const {Member} = require ('../models/member.js')
+
+async function getMemberById(req, res) {
+
+    const {id} = req.params 
+    const member = await Member.findByPk(id).catch(err => console.log(err))
+    if(!member) return res.status(404).json( {ok: false})
+    return res.status(200).json({member})
+}
+
+async function getAllMembers(req, res) { 
+    
+    const members = await Member.findAll().catch(err => console.log(err))
+    if(!members) return res.status(404).json( {ok: false})
+    return res.status(200).json({ members})
+}
+
+async function deleteMember(req, res) {
+
+    const {id} = req.body
+    if(!id) return res.status(400).send("no id found")
+    const isDeleted = await Member.destroy({
+        where: {
+            id: id
+        }
+    }).catch(err => console.log(err))
+    if(!isDeleted) return res.status(400).send("deletion failed").json({ok: false})
+    return res.status(200).json({ok: false})
+
+}
+
+async function restoreMember(req, res){
+
+    const {id} = req.body
+    if(!id) return res.status(400).send("no id found")
+    const isRestored = await Member.restore({
+        where: {
+            id: id
+        }
+    }).catch(err => console.log(err))
+    if(!isRestored) return res.status(400).send("restoring failed").json({ok: false})
+    return res.status(200).json({ok: false})
+}
+
+async function updateMember(req, res){
+
+    const {id, newValues} = req.body
+    const updated = await Member.update({...newValues}, {where: {id: id}}).catch(err => console.log(err))
+    if(!updated) return res.status(400).json({ok: false})
+    return res.status(200).send(updated)
+}
+
+module.exports = {
+
+    getAllMembers,
+    getMemberById,
+    deleteMember,
+    restoreMember,
+    updateMember
+
+}
