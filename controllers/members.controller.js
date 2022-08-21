778a3462f0078604429a7ec92b3d87deead9f078
Pablo Clavijo <76256398+pablodclavijo@users.noreply.github.com>
2022-08-21 13:23:56 -0300

    createMember added to controllers

--- controllers/members.controller.js
+++ controllers/members.controller.js
@@ -50,12 +50,21 @@ async function updateMember(req, res){
     return res.status(200).send(updated)
 }
 
+async function createMember(req, res){
+
+    const {newValues} = req.body
+    const created = await Member.create({...newValues}).catch(err => console.log(err))
+    if(!created) return res.status(400).json({ok: false})
+    return res.status(200).send(created)
+}
+
 module.exports = {
 
     getAllMembers,
     getMemberById,
     deleteMember,
     restoreMember,
-    updateMember
+    updateMember,
+    createMember
 
 }
