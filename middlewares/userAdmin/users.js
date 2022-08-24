a67e05f9ead03548dd28315fd5df6937b86b636b
Julian Moreyra <julianmoreyra.192@gmail.com>
2022-08-24 14:07:02 -0300

    Middleware de usuar

---
+++ middlewares/userAdmin/users.js
@@ -0,0 +1,38 @@
+const jwt = require('jsonwebtoken')
+// importo configuracion token (pablo covijo)
+
+/******************************************
+
+    Middleware authenticated user
+    
+******************************************/
+
+const AuthUser = async (req, res, next) => {
+
+    try {
+        // Mensaje de error param id != token user id
+        if (!req.headers.authorization) {
+            res.status(403).json({ message: "Authentication failed! Ivalid token" });
+            return;
+        }
+        //comprueba la validez del token con el ID del user
+        const token = req.headers.authorization.split(" ").pop();
+        const tokenUser = await verifyToken(token);
+
+        if (!tokenUser.id) {
+            res.status(403).json({ message: "Authentication failed! Invalid Token - User not found" });
+        }
+        else {
+            next();
+            console.log(decoded) //veo el usario sacar luego, consultar por winston 
+        }
+    }
+    catch (error) {
+        res.status(403).json({
+            message: 'Authentication failed! Token required'
+        });
+        console.log(error)
+    }
+}
+
+module.exports = AuthUser;
