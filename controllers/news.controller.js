243744b5234c24c699cad4baf4c15e38d2bc853d
Julian Moreyra <julianmoreyra.192@gmail.com>
2022-08-25 16:36:42 -0300

    News controllers model migration fixed

---
+++ controllers/news.controller.js
@@ -0,0 +1,100 @@
+const models = require('../models');
+const { News } = models
+
+// GET ALL NEWS
+
+const getAllNews = async (req, res) => {
+
+    let news = {};
+
+    try {
+        news = await News.findAll();
+        res.json(news)
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+        });
+
+    };
+}
+
+// CREATE NEWS
+
+const createNews = async (req, res, next) => {
+
+    let news = {};
+    const {
+        name,
+        content,
+        image,
+        categoryId
+    } = req.body;
+
+    try {
+        news = await News.create({
+            name,
+            content,
+            image,
+            categoryId
+        });
+        res.json(news)
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+        });
+
+    };
+};
+
+// UPDATE NEWS
+const updateNews = async (req, res) => {
+
+    let news = {};
+    const { id } = req.params;
+    const {
+        name,
+        content,
+        image,
+        categoryId
+    } = req.body;
+
+    try {
+        const article = await News.findOne({ where: { id } });
+        news = await article.update({
+            name,
+            content,
+            image,
+            categoryId
+        });
+        res.json(news)
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+        });
+    };
+};
+
+// DELETE NEWS
+const deleteNews = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const news = await News.destroy({ where: { id: +id } });
+        res.status(201).json({
+            message: `the New with ID=${news} was deleted`,
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+        });
+    };
+}
+
+
+module.exports = {
+    getAllNews,
+    createNews,
+    updateNews,
+    deleteNews
+}
---
+++ models/news.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class News extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // crear asociación con el nombre categoryId, esperar modelo catedory creado por Esteban
+    }
+  };
+  News.init({
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    content: {
+      allowNull: false,
+      type: DataTypes.TEXT
+    },
+    image: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    categoryId: {
+      allowNull: false,
+      type: DataTypes.INTEGER
+    }
+  }, {
+    sequelize,
+    modelName: 'News',
+    freezeTableName: true,
+  });
+  return News;
+};
