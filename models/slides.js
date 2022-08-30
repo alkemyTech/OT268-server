4acbcba1a7642d801efce97ec353e9c914019733
Matías Monastirsky <81826117+MatiMonas@users.noreply.github.com>
2022-08-29 21:01:23 -0300

    creates model and migrations of Slides

---
+++ models/slides.js
@@ -0,0 +1,28 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Slide extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Slide.belongsTo(models.Organization, { as: 'organization' });
+    }
+  }
+  Slide.init(
+    {
+      text: DataTypes.STRING,
+      imageUrl: DataTypes.STRING,
+      order: DataTypes.INTEGER,
+      organizationId: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: 'Slides',
+      paranoid: true,
+    },
+  );
+  return Slide;
+};
