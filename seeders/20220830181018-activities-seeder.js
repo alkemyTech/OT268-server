bc8a41aab4707958080055bf2b247c14aa32e5e2
sebitacasa <seba.-14@hotmail.com>
2022-08-31 09:58:24 -0300

    activities seeds added

---
+++ seeders/20220830181018-activities-seeder.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Activities', [{
+      name: 'Demo',
+      content: "Demo Category",
+      image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+      createdAt: new Date,
+      updatedAt: new Date
+    }], {});
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
