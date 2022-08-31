c3377b84a96a00435972ad8ba1016f45442eb8fd
briangvazq <briangvazq@gmail.com>
2022-08-31 05:54:58 -0300

    Add tokenValidation on Category

--- routes/category.js
+++ routes/category.js
@@ -1,23 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+const { adminAuthentication } = require('../middlewares/adminAuth/userAdmin');
 
-const CategoryController = require('../controllers/category.controller')
+const CategoryController = require('../controllers/category.controller');
 /* GET users listing. */
+
+router.use(adminAuthentication);
 
 router.get('/', CategoryController.get);
 
 // CREATE
-router.post("/", CategoryController.create);
-  
-  // Get By Id
-  router.get("/:id", CategoryController.getById);
-  
-  //UPDATE
-  router.put("/:id", CategoryController.update);
-  
-  //DELETE
-  router.delete("/:id", CategoryController.deleteById);
-  
+router.post('/', CategoryController.create);
+
+// Get By Id
+router.get('/:id', CategoryController.getById);
+
+//UPDATE
+router.put('/:id', CategoryController.update);
+
+//DELETE
+router.delete('/:id', CategoryController.deleteById);
 
 module.exports = router;
