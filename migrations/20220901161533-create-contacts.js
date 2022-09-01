60903ce5d64ac4770ba3bfa92ebea61b6f6f6997
sebitacasa <seba.-14@hotmail.com>
2022-09-01 13:23:52 -0300

    modelo y migracion de contactos creadas

---
+++ migrations/20220901161533-create-contacts.js
@@ -0,0 +1,43 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Contacts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      phone: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      message: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      deletedAt: {
+        type: Sequelize.DATE,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Contacts');
+  }
+};
--- models/slides.js
+++ models/slides.js
@@ -7,9 +7,9 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      Slide.belongsTo(models.Organization, { as: 'organization' });
-    }
+    // static associate(models) {
+    //   Slide.belongsTo(models.Organization, { as: 'organization' });
+    // }
   }
   Slide.init(
     {
