705080e9e00957f6b14f4968918fa42b4d786d35
sebitacasa <seba.-14@hotmail.com>
2022-09-05 10:58:28 -0300

    cambios en manejador de errores de controlador auth

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -13,8 +13,11 @@ const logIn = async (req, res, next) => {
     }
 
     const user = await User.findOne({ where: { email: email.toLowerCase() } });
+    if (!user) {
+           return res.status(404).json({ ok: false });
+        }
 
-    if (user && (await bcrypt.compare(password, user.password))) {
+     if (user && (await bcrypt.compare(password, user.password))) {
       let token = jwt.sign(
         { user_id: user.id, email, user_roleId: user.roleId },
         process.env.JWT_SECRET,
@@ -28,7 +31,7 @@ const logIn = async (req, res, next) => {
     }
     //next()
   } catch (error) {
-    res.status(404).send("Ups something happens");
+    return res.status(404).json({ ok: false });
   }
 };
 
