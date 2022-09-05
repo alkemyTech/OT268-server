5e2e58f7a8139bdfa08d724b7e32bbbcc4e510e7
Esteban <contacto@egdesarrollos.com.ar>
2022-09-05 09:58:54 -0300

    Change a findall arguments

--- controllers/slides.controller.js
+++ controllers/slides.controller.js
@@ -4,8 +4,8 @@ const Slides = require('../models/slides')(db.sequelize, db.Sequelize.DataTypes)
 
 
 const getAllSlides =  async (req,res) =>{
-    const id = req.params.id;
-    Slides.findAll({include:{model: organization, where: {organizationId:id}}}).then(response =>   
+    const id = req.params.organization_id;
+    Slides.findAll({ where: {organizationId:id}, order: [['order', 'ASC'],]}).then(response =>   
          res.status(200).send(response)
          ).catch(err => {
             res.status(500).send(err);
