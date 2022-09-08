ba346feaa491781a65ea8d9986299e46fe08b9db
pablodclavijo <76256398+pablodclavijo@users.noreply.github.com>
2022-09-07 22:01:06 -0300

    bugs + typos

--- controllers/members.controller.js
+++ controllers/members.controller.js
@@ -1,17 +1,17 @@
-const {Member} = require ('../models/member.js')
-
+const models = require('../models');
+const { Members } = models
 async function getMemberById(req, res) {
 
     const {id} = req.params 
-    const member = await Member.findByPk(id).catch(err => console.log(err))
+    const member = await Members.findByPk(id).catch(err => console.log(err))
     if(!member) return res.status(404).json( {ok: false})
     return res.status(200).json({member})
 }
 
 async function getAllMembers(req, res) { 
     
-    const members = await Member.findAll().catch(err => {return res.status(500).send(err)})
-    if(!members) return res.status(404).json( {ok: false})
+    const members = await Members.findAll({}).catch(err => {return res.status(500).send(err)})
+    if(!members.length) return res.status(404).json( { status: 404, ok: false})
     return res.status(200).json({ members})
 }
 
@@ -19,7 +19,7 @@ async function deleteMember(req, res) {
 
     const {id} = req.params
     if(!id) return res.status(400).send("no id found")
-    const member = await Member.findByPk(id).catch(err => {return res.status(500).send(err)})
+    const member = await Members.findByPk(id).catch(err => {return res.status(500).send(err)})
     if(!member) return res.status(400).send("member doesn't exist")
     const isDeleted = await member.destroy().catch(err => {return res.status(500).send(err)})
     if(!isDeleted) return res.status(400).send("deletion failed").json({ok: false})
@@ -31,7 +31,7 @@ async function restoreMember(req, res){
 
     const {id} = req.body
     if(!id) return res.status(400).send("no id found")
-    const isRestored = await Member.restore({
+    const isRestored = await Members.restore({
         where: {
             id: id
         }
@@ -43,7 +43,7 @@ async function restoreMember(req, res){
 async function updateMember(req, res){
 
     const {id, newValues} = req.body
-    const updated = await Member.update({...newValues}, {where: {id: id}}).catch(err => console.log(err))
+    const updated = await Members.update({...newValues}, {where: {id: id}}).catch(err => console.log(err))
     if(!updated) return res.status(400).json({ok: false})
     return res.status(200).send(updated)
 }
@@ -52,7 +52,7 @@ async function createMember(req, res){
 
     const {newMember} = req.body
     if(!newMember.name || typeof newMember.name != 'string') return res.status(400).send("name has to be a non-empty string")
-    const created = await Member.create({...newValues}).catch(err => {return res.status(500).send(err)})
+    const created = await Members.create({...newMember}).catch(err => {return res.status(500).send(err)})
     if(!created) return res.status(400).json({ok: false})
     return res.status(200).send(created)
 }
