1c0e1a8f964464d07288bea68877222fa7d3c386
Sebastian Urquijo <sebastianurquijo@hotmail.com>
2022-09-14 13:55:49 -0500

    First commit

--- controllers/comments.controller.js
+++ controllers/comments.controller.js
@@ -33,9 +33,33 @@ const uploadComment = async (req, res) => {
     }
   };
 
+  const updateComments = async (req, res) => {
+    const { id } = req.params;
+    const { userId, body, newsId } = req.body;
+  
+    try {
+      const comment = await Comment.findOne({ where: { id } });
+  
+      if (!comment) throw res.status(404).send(`Id ${id} not found`);
+  
+      changes = await comment.update({
+        userId,
+        body,
+        newsId,
+      });
+      return res.status(200).json(changes);
+  
+    } catch (error) {
+      return res.status(500).json({
+        message: error.message,
+      });
+    };
+  }
+
 module.exports = {
     getAllComments,
     uploadComment,
-    getCommentsByPostId
+    getCommentsByPostId,
+    updateComments
 };
   
