03529a314956d14e047067bc522445ba613ede8f
pablodclavijo <76256398+pablodclavijo@users.noreply.github.com>
2022-09-15 10:22:34 -0300

    ot268/84 endpoint eliminacion comentario

--- routes/comments.js
+++ routes/comments.js
@@ -4,7 +4,9 @@ const router = express.Router();
 const { checkRol } = require('../middlewares/checkRol/checkRol');
 
 const CommentsController = require('../controllers/comments.controller');
+const { deleteComment } = require('../controllers/comment.controllers');
 
 /* GET users listing. */
 
 router.get('/', checkRol, CommentsController.getAllComments);
+router.delete('/:id', deleteComment)
